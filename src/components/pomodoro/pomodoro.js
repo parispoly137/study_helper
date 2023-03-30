037eb63ad86543ed3f040bed761bd441d6795f48
parispoly137 <tkdgns6578@naver.com>
2023-03-30 20:06:28 +0900

    first commit

---
+++ src/components/pomodoro/pomodoro.js
@@ -0,0 +1,7 @@
+const title = document.querySelector(".pomodoro__title");
+
+
+
+title.innerText = "Pomodoro";
+
+console.log(title.innerText);
---
+++ src/components/toDoList/toDoList.js
@@ -0,0 +1,3 @@
+const toDoListTitle = document.querySelector(".todolist__title");
+
+toDoListTitle.innerText = "To Do List";
