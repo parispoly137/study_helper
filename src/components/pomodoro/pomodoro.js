0c903bae5d04fe3192f143e2672b1d6154645f50
parispoly137 <tkdgns6578@naver.com>
2023-03-31 22:18:48 +0900

    Initial settings

--- src/components/pomodoro/pomodoro.js
+++
@@ -1,7 +0,0 @@
-const title = document.querySelector(".pomodoro__title");
-
-
-
-title.innerText = "Pomodoro";
-
-console.log(title.innerText);
--- src/components/toDoList/toDoList.js
+++
@@ -1,3 +0,0 @@
-const toDoListTitle = document.querySelector(".todolist__title");
-
-toDoListTitle.innerText = "To Do List";
