420b9a8e5154cac74fc0d8c782754da80c12ae50
hy <gudnam33@naver.com>
2023-04-04 18:28:56 +0900

    Add checkbox and delete functions and functions to keep the todo item on refresh 1) Add checkbox
    2) Update css internal html with modify btn and delete btn
    
    3) Add a function to delete a todo item(li) when an event that clicks delete button occurs
    
    4) Add a function that maintains the previously entered todo items in the event of a reload

--- src/components/todo-list/addToDo/addToDo.js
+++ src/components/todo-list/addToDo/addToDo.js
@@ -2,21 +2,39 @@ const addingToDoForm = document.querySelector(".todolist__adding-form");
 const addingToDoInput = document.querySelector(".todolist__adding-form input");
 const items = document.getElementById("todolist__items")
 
+const TODOS_KEY = "todos";
+
+let toDos = []; //const로 하면 parsed로 인해 변수가 변경되는 내용을 받지 못한다.
+
+const saveToDos = () => {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+
+const deleteToDo = (event) => {
+    const li = event.target.closest("li");
+    li.remove();
+}
+
 const paintToDo = (newTodo) =>{
     const newItem = document.createElement("li");
-    newItem.innerHTML = `<input class="item" type="text" value="${newTodo}"/>
-    <button class="ItemIcon">
-        <span class="material-symbols-outlined">edit</span>
-        <span class="material-symbols-outlined">delete</span>
-    </button>`;
-    /*    const newItemInput = document.createElement("input");
-    newItemInput.type="text";
-    const newItemIcon = document.createElement("span"); */
-    
-    /*newItemIcon.className = ""; /*아이콘 입력해줘야함*/ 
-    
+    const newItemId=Date.now();
+    newItem.id=newItemId;
+    newItem.innerHTML = `
+    <input class="itemCheckbox" type="checkbox">
+    <input class="itemInput" type="text" disabled value="${newTodo}"/>
+    <button class="itemBtn--modify">
+        <span class="material-symbols-outlined item__modify-btn">edit</span>
+    </button>
+    <button class="itemBtn--delete">
+        <span class="material-symbols-outlined item__delete-btn">delete</span>
+    </button>
+    `;
 
+    const itemBtnDelete = newItem.querySelector(".itemBtn--delete")
+    itemBtnDelete.addEventListener("click", deleteToDo);
     items.appendChild(newItem);
+    
 }
 
 
@@ -24,8 +42,17 @@ const handleSubmit = (event) =>{
     event.preventDefault();
     const newTodo = addingToDoInput.value;
     addingToDoInput.value = "";
+    toDos.push(newTodo); //toDos 배열에 newTodo를 push 함
     paintToDo(newTodo);
+    saveToDos();
 };
 
 addingToDoForm.addEventListener("submit", handleSubmit);
 
+const savedToDos = localStorage.getItem(TODOS_KEY); /*저장된 todo를 가져오는거므로 getItem*/ 
+
+if (savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
