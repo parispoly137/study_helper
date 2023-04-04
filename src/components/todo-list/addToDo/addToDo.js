ff224fdc7f7bf828fcd5ab2c95f98df201203a35
hy <gudnam33@naver.com>
2023-04-04 17:03:34 +0900

    Create an item that receives and input value in toDoitems 1) Create 'todolist__items-form' (form) and 'todolist__item' (ul) within HTML
    2) Create an input and button for item(li) that receives an input value from addToDo in toDoits via the paintToDo function

--- src/components/todo-list/addToDo/addToDo.js
+++ src/components/todo-list/addToDo/addToDo.js
@@ -1,10 +1,30 @@
 const addingToDoForm = document.querySelector(".todolist__adding-form");
 const addingToDoInput = document.querySelector(".todolist__adding-form input");
+const items = document.getElementById("todolist__items")
+
+const paintToDo = (newTodo) =>{
+    const newItem = document.createElement("li");
+    newItem.innerHTML = `<input class="item" type="text" value="${newTodo}"/>
+    <button class="ItemIcon">
+        <span class="material-symbols-outlined">edit</span>
+        <span class="material-symbols-outlined">delete</span>
+    </button>`;
+    /*    const newItemInput = document.createElement("input");
+    newItemInput.type="text";
+    const newItemIcon = document.createElement("span"); */
+    
+    /*newItemIcon.className = ""; /*아이콘 입력해줘야함*/ 
+    
+
+    items.appendChild(newItem);
+}
+
 
 const handleSubmit = (event) =>{
     event.preventDefault();
     const newTodo = addingToDoInput.value;
     addingToDoInput.value = "";
+    paintToDo(newTodo);
 };
 
 addingToDoForm.addEventListener("submit", handleSubmit);
