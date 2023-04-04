6462f07c7bdbdd03cde51ec9284853b5f5217707
hy <gudnam33@naver.com>
2023-04-04 11:34:27 +0900

    Add empty content function on submit and create the toDoItems folder 1) Add empty content function on submit to addToDo
    2) Create the toDoItems folder and js, css files and connect to html

--- src/components/todo-list/addToDo/addToDo.js
+++ src/components/todo-list/addToDo/addToDo.js
@@ -3,6 +3,8 @@ const addingToDoInput = document.querySelector(".todolist__adding-form input");
 
 const handleSubmit = (event) =>{
     event.preventDefault();
+    const newTodo = addingToDoInput.value;
+    addingToDoInput.value = "";
 };
 
 addingToDoForm.addEventListener("submit", handleSubmit);
