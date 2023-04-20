2e03c9d1b25ec277aabe6546cb7c7fefb939f1c0
hy <gudnam33@naver.com>
2023-04-20 17:05:50 +0900

    Refactor: Move CSS code and reorganize files and folders 1) Move the CSS code to match the file name
    2) Rename the file from addToDo to mainInput
    
    3) Create a subfolder in the toDoHandler folder, and move the code into it

--- src/components/todo-list/mainInput/mainInput.js
+++ src/components/todo-list/mainInput/mainInput.js
@@ -1,3 +1,26 @@
+/**addingToDoInput에서 submit이 발생했을 때 toDos 배열에 value와 localStorage에 toDos를 넣고
+ * todo를 형성하는 함수 */
+const handleSubmit = (event, inputData) =>{
+    event.preventDefault();
+    if (inputData.value==false){
+        alert("내용을 입력해주세요.")
+    }
+    else {
+    const newTodo = inputData.value.trim();
+    inputData.value = "";
+    const newToDoObj = { //object 형식으로 data를 저장해줌
+        text:newTodo,
+        id: Date.now(),
+    }
+    toDos.push(newToDoObj); //toDos 배열에 newTodoObj를 push 함
+    paintToDo(newToDoObj);
+    saveToDos();
+    }
+};
+
+addingToDoForm.addEventListener("submit", (event) => handleSubmit(event, addingToDoInput));
+
+
 /** todo를 화면에 그려주는 함수 */
 const paintToDo = (newTodo) =>{
     const newItem = document.createElement("li");
